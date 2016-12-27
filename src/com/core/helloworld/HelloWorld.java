e481c09fa97e316f14e05da44e5a40679b215b27
AndreySotnikovQA <mygitrepozitory@gmail.com>
2016-12-26 17:21:03 -0800

    my  commit

--- src/com/core/helloworld/HelloWorld.java
+++ src/com/core/helloworld/HelloWorld.java
@@ -1,9 +1,13 @@
 package com.core.helloworld;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class HelloWorld {
 
-	public static void main(String[] args) {
-System.out.println("Hello World !!!");
+	public static void main(String[] args) {		
+		System.out.println("helloWorld");
+		
 	}
 
 }
